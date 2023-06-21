ebeb180f69b5a9d995d3b9f25a9f855e3cefab10
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-06-21 16:39:19 +0545

    DOM

--- DOM/js/index.js
+++ DOM/js/index.js
@@ -1,3 +1,8 @@
 // alert("If you Enter name will change ");
 // document.querySelector("h1").innerHTML="Gaurav";
 var num = document.getElementsByClassName('dice1');
+var randomNum = Math.floor(Math.random()*6 ) +1;
+
+var randomDice = "dice" + "randomNum" + ".png";
+
+var randomImage = "images/" + randomDice;
