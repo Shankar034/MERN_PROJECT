dca7f625bf8a2c23bb5cd67a1e5bf3ccf0f77fd6
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-07-03 15:09:31 +0545

    DOM

--- Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -1,6 +1,23 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 
+
+
+
+for (var i = 0; i< numberOfDrumButtons; i++){
+    document.querySelectorAll('.drum')[i].addEventListener('click',function(){
+        this.style.color="Blue";
+    });
+}
+
+var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
+
+
+
+
+/*
+
 function multiply(val1, val2){
     return val1 * val2;
 }
@@ -18,12 +35,4 @@ function calculate( val1, val2,  operator){
     return operator (val1, val2);
 }
 calculate(3, 4,subtract);
-
-
-
-for (var i = 0; i< numberOfDrumButtons; i++){
-    document.querySelectorAll('.drum')[i].addEventListener('click',function(){
-        console.log("I got clicked")
-    });
-}
-
+*/
