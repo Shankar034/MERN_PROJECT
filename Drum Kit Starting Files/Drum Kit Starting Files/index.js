b48e30a7860d9fa0c9b6dd3c011b2373b3cf0475
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-07-03 14:02:12 +0545

    DOM

--- Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -1,11 +1,29 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
+
+function multiply(val1, val2){
+    return val1 * val2;
+}
+function add (val1, val2){
+    return val1+val2;
+}
+function subtract(val1, val2){
+    return val1-val2;
+}
+function divide(val1, val2){
+    return val1/val2;
+}
+
+function calculate( val1, val2,  operator){
+    return operator (val1, val2);
+}
+calculate(3, 4,subtract);
+
+
+
 for (var i = 0; i< numberOfDrumButtons; i++){
     document.querySelectorAll('.drum')[i].addEventListener('click',function(){
-
+        console.log("I got clicked")
     });
 }
 
-document.querySelectorAll('.drum')[i].addEventListener('click', function(){
-    
-})
